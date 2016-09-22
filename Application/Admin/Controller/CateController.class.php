b5a4ebab72dd21b0e78a5a2c703595fb14468c5a
d4smart <d4smart@foxmail.com>
2016-09-22 10:55:18 +0800

    第一次提交，已添加后台和前台模板，分离后台公共文件，同时创建了数据表

---
+++ Application/Admin/Controller/CateController.class.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Desp:
+ * User: d4smart
+ * Date: 2016/9/22
+ * Time: 9:58
+ * Email:   d4smart@foxmail.com
+ * Github:  https://github.com/d4smart
+ */
+namespace Admin\Controller;
+use Think\Controller;
+
+class CateController extends Controller
+{
+    public function lst() {
+        $this->display();
+    }
+
+    public function add() {
+        $this->display();
+    }
+
+    public function edit() {
+        $this->display();
+    }
+
+    public function del() {
+
+    }
+}
