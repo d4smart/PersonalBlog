bbbd3bd535362770bb9cf17e7ef603e3db6a07e4
d4smart <d4smart@foxmail.com>
2016-09-30 11:30:29 +0800

    上一篇下一篇

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -17,11 +17,19 @@ class ArticleController extends CommonController
         $arts = D('article')->find(I('id'));
         $this->assign('arts', $arts);
         $this->catename($arts['cateid']);
+        $this->other(I('id'));
         $this->display();
     }
 
     public function catename($cateid) {
         $cate = D('cate')->find($cateid);
         $this->assign('catename', $cate['catename']);
+    }
+
+    public function other($id) {
+        $pre = D('article')->where('id<'.$id)->order('id desc')->find();
+        $next = D('article')->where('id>'.$id)->order('id asc')->find();
+        $this->assign('pre', $pre);
+        $this->assign('next', $next);
     }
 }
